21ffa1c47f5763d5b7e35ab39e5f591681d14833
JENNIFER MOREIRA RODRIGUES <jennifer.rodrigues39>
2025-03-24 20:41:02 -0300

    aula - conection

---
+++ src/controllers/usuários.js
@@ -0,0 +1,19 @@
+const db = require('../dataBase/connection');
+
+module.exports = {
+    async listaUsuarios(request, response){
+        try {
+            return response.status(200).json({
+                sucesso: true,
+                mensagem: 'Lista de usuários.',
+                dados: null
+            });
+        } catch (error){
+            return response.status(200).json({
+                sucesso: false,
+                mensagem: 'Erro na requisição.',
+                dados: error.message
+            });
+        }
+    }
+}
