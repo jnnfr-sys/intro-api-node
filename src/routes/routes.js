b79de9f8316de058b3bf7f9378bfd9ac3ed0d58d
JENNIFER MOREIRA RODRIGUES <jennifer.rodrigues39>
2025-03-28 20:41:23 -0300

    aula routes

---
+++ src/routes/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const BairroController = require('../controllers/bairro');
+
+router.get('/bairro', BairroController.listarBairro);
+
+module.exports = router;
